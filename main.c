30ea423ce7ae8bb101c056a069363452e4df891f
mhmtkrl <koral.mehmet45@gmail.com>
2021-06-26 17:54:28 +0300

    components were connected together

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include "stm32f4xx.h"                  // Device header
 
 int main() {
+	RCC->AHB1ENR |= 1ul << 3; 		//GPIOD Clock Enable
+	GPIOD->MODER |= 0x55000000;		//PD12, PD13, PD14, and PD15 set as an output
 	
+	GPIOD->ODR |= 1ul << 12;
 }
